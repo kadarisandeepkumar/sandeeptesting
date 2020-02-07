bf3c3876aa666f15cb9da765885b8f8dbdd69774
kadarisandeepkumar <kadarisandeep011@gmail.com>
2020-02-07 13:15:21 +0530

    flipkart

--- selenium/src/Com/selenium/firstclass.java
+++ selenium/src/Com/selenium/firstclass.java
@@ -8,7 +8,9 @@ public class firstclass {
 	public static WebDriver driver;
 	public static void main(String[] args) {
 		driver=new FirefoxDriver();
-		driver.get("https://www.amazon.com");
+		driver.get("https://www.flipkart.com");
+		
+		
 	}
 
 	}
